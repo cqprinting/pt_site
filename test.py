94cafb08fc19cb47d898035774b5c5dba063edee
Jake Peterson <jake@acogdev.com>
2015-11-06 06:42:22 -0700

    added test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class DummyTest(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def test_dummy(self):
+    	pass
+
+
+if __name__ == '__main__':
+    unittest.main()
